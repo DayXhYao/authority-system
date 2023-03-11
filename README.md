a2235605ca3845db4fb81c32e74e882358dcfae0
DayXhYao <1qaz@WSX>
2023-03-11 10:35:12 +0800

    init: project struct

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# authority-center
+# authority-system
 一个领域驱动设计的权限中心
---
+++ authority-system-core/src/main/java/com/day/authority/core/A.java
@@ -0,0 +1,8 @@
+package com.day.authority.core;
+
+/**
+ * @author DayXhYao
+ * @date 2023/3/11 10:34
+ */
+public class A {
+}
